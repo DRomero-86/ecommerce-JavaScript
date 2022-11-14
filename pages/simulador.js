9929d66f1e8d068e7d20a0b13eb9ee1caf839db8
damiandrom86 <damian.alexis.romero@gmail.com>
2022-11-14 01:56:03 -0300

    se creo el simulador de heladeria

---
+++ pages/simulador.js
@@ -0,0 +1,32 @@
+let nombre = prompt("Ingresa tu nombre");
+let dinero = parseInt(prompt("Hola " + nombre + " Bienvenido a la heladería. Los precios son:" +  "\n" + 
+"Palito: $5" +  "\n" + "Cucurucho: $10" + "\n" + "Tacita: $15" +  "\n" + "Pote 1/4: $20" +  "\n" + "¿cuanto dinero($) dispones:"));
+let dineroDiferencia;
+
+if (dinero < 5){
+    console.log(nombre + " Debes tener más dinero. El helado más barato es de $5");
+} else if (dinero > 4 && dinero < 10){
+    dineroDiferencia = (dinero - 5);
+    if (dinero == 5){
+    console.log(nombre + " te alcanza para un helado de palito");
+    } else{
+    console.log(nombre + " te alcanza para un helado de palito y te sobran $" + dineroDiferencia);
+}} else if (dinero > 9 && dinero < 15){
+    dineroDiferencia = (dinero - 10);
+    if (dinero == 10){
+    console.log(nombre + " te alcanza para un helado de cucurucho");
+    } else {
+    console.log(nombre + " te alcanza para un helado de cucurucho y te sobran $" + dineroDiferencia);
+}} else if (dinero > 14 && dinero < 20){
+    dineroDiferencia = (dinero - 15);
+    if (dinero == 15){
+    console.log(nombre + " te alcanza para un helado de Tacita");
+    } else {
+    console.log(nombre + " te alcanza para un helado de Tacita y te sobran: $" + dineroDiferencia);
+}} else if (dinero > 19){
+    dineroDiferencia = (dinero - 20);
+    if (dinero == 20){
+    console.log(nombre + " te alcanza para 1/4 kilo de helado");
+    } else {    
+    console.log(nombre + " te alcanza para un 1/4 kilo de helado y te sobran: $" + dineroDiferencia);
+}}
